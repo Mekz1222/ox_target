a127c4d94a147dabb5cc9e3c65b681b04b8befd6
Mekz1222 <53654750+Mekz1222@users.noreply.github.com>
2022-11-29 17:36:13 +0200

    Update main.js

--- web/js/main.js
+++ web/js/main.js
@@ -10,25 +10,6 @@ window.addEventListener('message', (event) => {
   switch (event.data.event) {
     case 'visible': {
       body.style.visibility = event.data.state ? 'visible' : 'hidden';
-      return (eye.style.fill = 'black');
-    }
-
-    case 'leftTarget': {
-      return (eye.style.fill = 'black');
-    }
-
-    case 'setTarget': {import { createOptions } from './createOptions.js';
-
-const optionsWrapper = document.getElementById('options-wrapper');
-const body = document.body;
-const eye = document.getElementById('eyeSvg');
-
-window.addEventListener('message', (event) => {
-  optionsWrapper.innerHTML = '';
-
-  switch (event.data.event) {
-    case 'visible': {
-      body.style.visibility = event.data.state ? 'visible' : 'hidden';
 
       return (eye.style.fill = 'url(#gradient)');
     }
@@ -39,19 +20,6 @@ window.addEventListener('message', (event) => {
 
     case 'setTarget': {
       eye.style.fill = 'url(#gradient2)';
-
-      if (event.data.options) {
-        for (const type in event.data.options) {
-          event.data.options[type].forEach((data, id) => {
-            createOptions(type, data, id + 1);
-          });
-        }
-      }
-    }
-  }
-});
-
-      eye.style.fill = '#cfd2da';
 
       if (event.data.options) {
         for (const type in event.data.options) {

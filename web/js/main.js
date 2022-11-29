0c7413dcea47c19db61923b703f86b8324812250
Mekz1222 <53654750+Mekz1222@users.noreply.github.com>
2022-11-29 17:15:11 +0200

    Update main.js

--- web/js/main.js
+++ web/js/main.js
@@ -17,7 +17,40 @@ window.addEventListener('message', (event) => {
       return (eye.style.fill = 'black');
     }
 
+    case 'setTarget': {import { createOptions } from './createOptions.js';
+
+const optionsWrapper = document.getElementById('options-wrapper');
+const body = document.body;
+const eye = document.getElementById('eyeSvg');
+
+window.addEventListener('message', (event) => {
+  optionsWrapper.innerHTML = '';
+
+  switch (event.data.event) {
+    case 'visible': {
+      body.style.visibility = event.data.state ? 'visible' : 'hidden';
+
+      return (eye.style.fill = 'url(#gradient)');
+    }
+
+    case 'leftTarget': {
+      return (eye.style.fill = 'url(#gradient)');
+    }
+
     case 'setTarget': {
+      eye.style.fill = 'url(#gradient2)';
+
+      if (event.data.options) {
+        for (const type in event.data.options) {
+          event.data.options[type].forEach((data, id) => {
+            createOptions(type, data, id + 1);
+          });
+        }
+      }
+    }
+  }
+});
+
       eye.style.fill = '#cfd2da';
 
       if (event.data.options) {
